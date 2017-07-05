00067e45f4e60145c74829b6d27214d49835ab5a
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2017-07-05 15:46:50 -0300

    Fix in before-deploy.sh script.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-![alt text](https://travis-ci.org/netshoes/spring-cloud-sleuth-amqp-starter.svg?branch=master "Logo Title Text 1")
+![Build status](https://travis-ci.org/netshoes/spring-cloud-sleuth-amqp-starter.svg?branch=master "Logo Title Text 1")
 
 # Motivation
 The project spring-cloud-sleuth does not provided a instrumentation for spring-rabbit. In this project we implement this instrumentation.
--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_4c61ff45a8d9_key -iv $encrypted_4c61ff45a8d9_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
